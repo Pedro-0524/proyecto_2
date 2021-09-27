ffab19cb04bbea6cb2c4f765c7d46292443700d1
Pedro Soto <pedropablo.soto.y@gmail.com>
2021-09-27 13:53:34 -0300

    proyecto_2

---
+++ lib/pages/juegos.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class juegos extends StatelessWidget {
+  const juegos({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("juegos"),
+    );
+  }
+}
